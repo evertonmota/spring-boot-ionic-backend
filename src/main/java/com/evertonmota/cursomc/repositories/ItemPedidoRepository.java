d662d03bf1f263cb13ab339b63a880e412ab6fa3
Everton Mota <mcostagt@gmail.com>
2021-02-23 07:45:32 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/evertonmota/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.evertonmota.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.evertonmota.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
