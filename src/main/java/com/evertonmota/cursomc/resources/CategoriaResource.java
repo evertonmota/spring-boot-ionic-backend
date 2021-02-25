cafaa8918a90f3f8868859d1a284e01450ba2ec7
Everton Mota <mcostagt@gmail.com>
2021-02-25 08:22:05 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/evertonmota/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/evertonmota/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.evertonmota.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.evertonmota.cursomc.domain.Categoria;
 import com.evertonmota.cursomc.services.CategoriaService;
@@ -21,5 +25,22 @@ public class CategoriaResource {
 	public ResponseEntity<?> find (@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return  ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	// @RequestBody  Faz com que o JSON seja convertido por objeto java automaticamente.
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		// A operação save do repository, retorna um object.
+		obj = service.insert(obj); 
+
+		// recuperar um novo id e fornecer como argumento da URI.
+		// fromCurrentRequest() = http://127.0.0.1:8080/categorias 
+		// .path("/{id}"); /3
+		
+		URI url = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(url).build();
+		 
 	}
 }
