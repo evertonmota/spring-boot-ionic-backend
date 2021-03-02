2e8f5984e06f72aa78d85f443e47ceebca8a957f
Everton Mota <mcostagt@gmail.com>
2021-03-02 10:28:55 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/evertonmota/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/evertonmota/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -34,7 +36,9 @@ public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.POST)
 	// @RequestBody  Faz com que o JSON seja convertido por objeto java automaticamente.
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
+		
+		Categoria obj = service.fromDTO(objDTO);
 		
 		// A operação save do repository, retorna um object.
 		obj = service.insert(obj); 
@@ -50,7 +54,9 @@ public class CategoriaResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody	Categoria obj , @PathVariable Integer id){
+	public ResponseEntity<Void> update(@Valid @RequestBody	CategoriaDTO objDTO , @PathVariable Integer id){
+		
+		Categoria obj = service.fromDTO(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
