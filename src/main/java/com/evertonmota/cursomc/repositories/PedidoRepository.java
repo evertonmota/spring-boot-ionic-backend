9e65904675191b385df122bb5e68a9ca77dd20dd
everton mota <everton.costa_hartb@prestador.globo>
2021-12-02 10:35:19 -0300

    Cliente so recupera seus pedidos

--- src/main/java/com/evertonmota/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/evertonmota/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.evertonmota.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.evertonmota.cursomc.domain.Cliente;
 import com.evertonmota.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
+	@Transactional
+	Page<Pedido> findByCliente (Cliente cliente, PageRequest page);
 }
--- src/main/java/com/evertonmota/cursomc/services/PedidoService.java
+++ src/main/java/com/evertonmota/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.evertonmota.cursomc.domain.Cliente;
 import com.evertonmota.cursomc.domain.ItemPedido;
 import com.evertonmota.cursomc.domain.PagamentoComBoleto;
 import com.evertonmota.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.evertonmota.cursomc.domain.enums.EstadoPagamento;
 import com.evertonmota.cursomc.repositories.ItemPedidoRepository;
 import com.evertonmota.cursomc.repositories.PagamentoRepository;
 import com.evertonmota.cursomc.repositories.PedidoRepository;
+import com.evertonmota.cursomc.security.UserSS;
+import com.evertonmota.cursomc.services.exceptions.AuthorizationException;
 import com.evertonmota.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -71,5 +77,20 @@ public class PedidoService {
 		return obj;
 		
 	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado.");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction), orderBy );
+		
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+
 
 }
